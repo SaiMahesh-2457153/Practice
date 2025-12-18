6dc60594dc4674ffc86ce782e3c883b923f6bc54
Devaruppula <2457153@cognizant.com>
2025-12-18 12:05:53 +0530

    html-file-added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  name:String="saimahesh";
+  age:number=22;
   protected readonly title = signal('paractice');
 }
